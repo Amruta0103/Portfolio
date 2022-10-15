7b6c4b7c791b4db4bf9d8a35a069d10427f534a7
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2022-10-15 20:17:54 +0530

    temp

---
+++ design-one/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+// import styled from 'styled'
+function App() {
+  // const Button = styled.button`
+  // background:${props => props.primary ? "lightblue": "white"};
+  // color:${props => props.primary ? "white" : "lightblue"};
+  // font-size: 2em;
+  // `
+  return (
+    <div className="App">
+      <button>Normal</button>
+    </div>
+  );
+}
+
+export default App;
