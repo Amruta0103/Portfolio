ce9e23b1aeb29968153eeb07179afbd31245e6dd
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2022-10-17 22:36:25 +0530

    temp

--- design-one/src/App.jsx
+++ design-one/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
-// import styled from 'styled'
+import styled from 'styled-components'
 function App() {
-  // const Button = styled.button`
-  // background:${props => props.primary ? "lightblue": "white"};
-  // color:${props => props.primary ? "white" : "lightblue"};
-  // font-size: 2em;
-  // `
+  const MainDiv = styled.div`
+  backgroundColor: "grey";
+  `;
+  const Button = styled.button`
+  background:${props => props.primary ? "lightblue": "white"};
+  color:${props => props.primary ? "white" : "lightblue"};
+  font-size: 2em;
+  `
   return (
-    <div className="App">
-      <button>Normal</button>
+    <div className='App'>
+      <MainDiv>
+      <Button primary>Normal</Button>
+      </MainDiv>
     </div>
   );
 }
