dfdca9eb2d609c7590f6804727f50a43b456fcaa
Amruta_D <71976347+Amruta0103@users.noreply.github.com>
2023-05-23 21:02:11 +0530

    final font

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,7 +7,6 @@ const AboutMe = () => {
         <MsgTitle>Hello!!</MsgTitle>
         I am Amruta,<br/> a Front-End Developer.<br/>
         I like creating web elements, that are efficient and easy to use.
-        <StackTitle>My Tech Stack</StackTitle>
       </Msg>
     </AboutMeBox>
   )
@@ -20,20 +19,15 @@ align-items: center;
 height: 30vh;
 width: 100%;
 margin: 3rem auto;
+font-size: 20px;
 `
 const Msg = styled.div`
 text-align: center;
 padding: 1rem;
-background: ${props => props.theme.accentColor};
 border-radius: 2rem ;
 `
 const MsgTitle = styled.div`
 font-size: 2rem;
 font-weight: 300;
-`
-const StackTitle = styled.div`
-font-size: 20px;
-font-weight: 300;
-margin: 1rem auto;
 `
 export default AboutMe;
